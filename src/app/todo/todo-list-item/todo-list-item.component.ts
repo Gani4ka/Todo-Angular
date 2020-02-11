a6c946882aadc4d47b5894aeb5b463ee45049d2a
Anya Efimova <gani4ka_n@ukr.net>
2020-02-12 01:22:59 +0200

    last handlers

--- src/app/todo/todo-list-item/todo-list-item.component.ts
+++ src/app/todo/todo-list-item/todo-list-item.component.ts
@@ -1,20 +1,26 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { TodoModel } from '../models/todo.model';
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { TodoModel } from "../models/todo.model";
 
 @Component({
-  selector: 'app-todo-list-item',
-  templateUrl: './todo-list-item.component.html',
-  styleUrls: ['./todo-list-item.component.css']
+  selector: "app-todo-list-item",
+  templateUrl: "./todo-list-item.component.html",
+  styleUrls: ["./todo-list-item.component.css"]
 })
 export class TodoListItemComponent {
   @Input() todo: TodoModel;
   @Output() onItemClick = new EventEmitter<string>();
+  @Output() onImportantClick = new EventEmitter<string>();
+  @Output() onDeleteClick = new EventEmitter<string>();
 
-  handleItemClick () {
+  handleItemClick() {
     this.onItemClick.emit(this.todo.id);
   }
 
-  isDone() {
-    this.todo.done
+  handleImportantClick() {
+    this.onImportantClick.emit(this.todo.id);
+  }
+
+  handleDeleteClick() {
+    this.onDeleteClick.emit(this.todo.id);
   }
 }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,59 +1,65 @@
-import { Component } from '@angular/core';
-import { TodoModel } from './models/todo.model';
-
+import { Component } from "@angular/core";
+import { TodoModel } from "./models/todo.model";
 
 const initialState: TodoModel[] = [
   {
-    label: '1',
+    label: "1",
     important: false,
     done: false,
-    id: 'i1'
+    id: "i1"
   },
   {
-    label: '2',
+    label: "2",
     important: false,
     done: false,
-    id: 'i2'
+    id: "i2"
   },
   {
-    label: '3',
+    label: "3",
     important: false,
     done: false,
-    id: 'i3'
+    id: "i3"
   }
 ];
 
 @Component({
-  selector: 'app-todo',
-  templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  selector: "app-todo",
+  templateUrl: "./todo.component.html",
+  styleUrls: ["./todo.component.css"]
 })
-
 export class TodoComponent {
-	todos = initialState;
+  todos = initialState;
 
   get doneCount(): number {
-  return this.todos.filter(
-      (el: TodoModel) => el.done
-    ).length;
-  };
+    return this.todos.filter((el: TodoModel) => el.done).length;
+  }
 
   get todoCount(): number {
-    return this.todos.length - this.doneCount
-  };
+    return this.todos.length - this.doneCount;
+  }
 
-  formHandler (data:string): void {
+  formHandler(data: string): void {
     const newItem = new TodoModel(data);
     this.todos = [...this.todos, newItem];
   }
 
-  itemHandler (data: string): void {
-   this.todos.map(item => {
-     if (item.id === data) {
-       item.done = !item.done
-     }
-   });
-   console.log(this.todos);
-  };
-  
+  itemHandler(data: string): void {
+    this.todos.map(item => {
+      if (item.id === data) {
+        item.done = !item.done;
+      }
+    });
+  }
+
+  importantHandler(data: string): void {
+    this.todos.map(item => {
+      if (item.id === data) {
+        item.important = !item.important;
+      }
+    });
+  }
+
+  deleteHandler(data: string): void {
+    this.todos = this.todos.filter(item => item.id !== data);
+  }
 }
